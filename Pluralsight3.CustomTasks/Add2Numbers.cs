6e9052f784e32fddd7816b016d5bb4547bac08b7
Thanh Duc Nguyen <thanhduc.nguyen@optimizely.com>
2024-09-05 17:03:24 +0700

    Pluralsight 2 and 3

---
+++ Pluralsight3.CustomTasks/Add2Numbers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Build.Framework;
+
+namespace Pluralsight3.CustomTasks
+{
+    public class Add2Numbers : Microsoft.Build.Utilities.Task
+    {
+        [Required]
+        public int Number1 { get; set; }
+
+        [Required]
+        public int Number2 { get; set; }
+
+        [Output]
+        public int Result { get; set; }
+
+        public override bool Execute()
+        {
+            Result = Number1 + Number2;
+            BuildEngine.LogMessageEvent(new BuildMessageEventArgs("Add 2 numbers", "Add", "Add2Numbers", MessageImportance.High));
+
+            return true;
+        }
+    }
+}
